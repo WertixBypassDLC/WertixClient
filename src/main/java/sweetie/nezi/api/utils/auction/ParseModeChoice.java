b13c7aabc1067f7e97896901af9351402a43213d
nedzi <nedzis_zelenskyi@proton.me>
2026-04-13 22:43:18 +0300

    13.04.26 22:43

--- src/main/java/sweetie/nezi/api/utils/auction/ParseModeChoice.java
+++ src/main/java/sweetie/nezi/api/utils/auction/ParseModeChoice.java
@@ -5,10 +5,7 @@ import sweetie.nezi.api.module.setting.ModeSetting;
 
 @AllArgsConstructor
 public enum ParseModeChoice implements ModeSetting.NamedChoice {
-    FUN_TIME("Fun Time"),
-    SPOOKY_TIME("Spooky Time"),
-    HOLY_WORLD("Holy World"),
-    REALLY_WORLD("Really World");
+    FUN_TIME("Fun Time");
 
     private final String name;
 
